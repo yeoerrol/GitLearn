9e91bd53db7132c676263a92d7ec65e69c432190
yeoerrol <errol.yeo@gmail.com>
2017-04-01 18:30:12 -0700

    Learning to first Commit

---
+++ Box Git/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+   public static void main(String[] args){
+	   GitLearn data = new GitLearn();
+	   
+	   displayGreeting();
+	   data.display();
+   }
+   
+   private static void displayGreeting(){
+	   System.out.println("Hello Learning Git");
+	   System.out.println("GIT GIT");
+	   System.out.print("Commit1");
+	   }
+}
